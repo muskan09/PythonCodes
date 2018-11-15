6f06e6dbab0c204585fa07d0b4d21c3c29a0594e
muskan09 <muskankalsi9@gmail.com>
2018-11-15 20:39:38 +0530

    binascii.py

---
+++ binascii.py
@@ -0,0 +1,11 @@
+import binascii
+x="oj20mk9"
+print (binascii.b2a_hex(x))
+import base64
+x
+b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+print (binascii.b2a_hex(x))
+print (base64.b64encode(x))
+y ="49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+encoded = HEX_STRING.decode("y").encode("base64")
+print (y)
